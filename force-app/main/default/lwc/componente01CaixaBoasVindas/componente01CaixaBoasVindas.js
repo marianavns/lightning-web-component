c8c7f8411472edeffd3d3d2643a2b7c495b05b6f
Mariana Souza <69424128+marianavns@users.noreply.github.com>
2024-08-02 17:48:44 -0300

    IMPORTANTE: Configure as funções para que elas façam o que você precisa

--- force-app/main/default/lwc/componente01CaixaBoasVindas/componente01CaixaBoasVindas.js
+++ force-app/main/default/lwc/componente01CaixaBoasVindas/componente01CaixaBoasVindas.js
@@ -5,5 +5,13 @@ export default class HelloWorld extends LightningElement {
     /* cria variável para guardar input */
         nomeInserido;
 
+    /* limpa a barra de input quando clica sobre ela */
+        limpaInput(event) {
+            this.nomeInserido = null;
+        }
 
+    /* captura tudo o que é escrito no input em tempo real */
+        capturaInput(event) {
+            this.nomeInserido = event.target.value;
+        }
 }
