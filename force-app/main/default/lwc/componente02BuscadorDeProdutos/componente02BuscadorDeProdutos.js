c238a503221a4011c5bc7bf60b264981bd717cec
Mariana Souza <69424128+marianavns@users.noreply.github.com>
2024-08-02 19:09:37 -0300

    Outro exemplo de LWC e eventos lançando informações em tela

---
+++ force-app/main/default/lwc/componente02BuscadorDeProdutos/componente02BuscadorDeProdutos.js
@@ -0,0 +1,28 @@
+import { LightningElement, api } from 'lwc';
+
+export default class BuscadorDeProdutos extends LightningElement {
+
+    // declaração variáveis
+    listaProdutos = ['camiseta', 'calça', 'bolsa', 'meia', 'sapato', 'cinto', 'boné', 'pijama', 'casaco', 'jaqueta']
+    @api produtoBuscado = ''
+    @api produtosRetornados = []
+
+
+    procuraProdutosNaLista(event){
+        
+        this.produtoBuscado = event.target.value // captura do input
+              
+        if (this.produtoBuscado != ''){
+            this.produtosRetornados = this.listaProdutos.filter(item => item.includes(this.produtoBuscado)); // busca de acordo com input
+        }
+
+        if (this.produtoBuscado == ''){
+            this.produtosRetornados = ['']
+        }
+
+        if (this.produtosRetornados.length == 0){
+            this.produtosRetornados = ['Nenhum produto encontrado!']
+        }
+        
+    }
+}
