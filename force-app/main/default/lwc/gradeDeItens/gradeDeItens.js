dc534c50dfbfded93f8771665d71c0df8576f044
Mariana Souza <69424128+marianavns@users.noreply.github.com>
2024-08-04 12:08:18 -0300

    Passo 2: Criar o javascript tratando os dados que serão exibidos, normalmente.

--- force-app/main/default/lwc/gradeDeItens/gradeDeItens.js
+++ force-app/main/default/lwc/gradeDeItens/gradeDeItens.js
@@ -1,3 +1,37 @@
 import { LightningElement } from 'lwc';
 
-export default class GradeDeItens extends LightningElement {}
+export default class GradeDeItens extends LightningElement {
+    
+    produtos = [
+        {
+            "id": 1,
+            "title": "Camiseta",
+            "img": "https://cdn.awsli.com.br/1960/1960866/produto/125390796/a12dd9ac46.jpg"
+        },
+        {
+            "id": 2,
+            "title": "Calça",
+            "img": "https://static.camisariafmw.com.br/produtograde/20230914115132_2540997460_GZ.jpg"
+        },
+        {
+            "id": 3,
+            "title": "Saia",
+            "img": "https://61007.cdn.simplo7.net/static/61007/sku/roupas-saia-feminina-colegial-xadrez-escoces--p-1629207948934.png"
+        },
+        {
+            "id": 4,
+            "title": "Short",
+            "img": "https://images.tcdn.com.br/img/img_prod/1134256/short_linho_verde_57_1_fc4b752fcdbf2bd724c8b40bf3cf0134.jpg"
+        },
+        {
+            "id": 5,
+            "title": "Jaqueta",
+            "img": "https://m.media-amazon.com/images/I/41BJmaVqmRL._AC_SY1000_.jpg"
+        },
+        {
+            "id": 6,
+            "title": "Vestido",
+            "img": "https://ladydress.com.br/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/l/a/ladydress-vestido-festa-madrinha--rosa---longo_5__5_2_.jpg"
+        }
+    ]
+}
